c8bcacadf802c9a14248c99f793a2aeff619d25f
Huib Keemink <huib.keemink@creativedutchmen.com>
2011-02-08 23:50:07 +0100

    Almost done! Just the delete (and cleanup.. Lots of cleanup)

--- content/content.templates.php
+++ content/content.templates.php
@@ -11,6 +11,11 @@ Class contentExtensionemail_templatestemplates extends AdministrationPage {
 		$this->_uri = URL . '/symphony/extension/email_templates';
 	}
 	
+	public function __actionNew(){
+		$this->_context[0] = 'new';
+		$this->__actionEdit();
+	}
+	
 	function __viewIndex(){
 		$this->setPageType('table');
 		$this->setTitle(__('Symphony &ndash; Email Templates'));
@@ -43,19 +48,23 @@ Class contentExtensionemail_templatestemplates extends AdministrationPage {
 				);
 				$col_name->appendChild(Widget::Input("items[" . ++$i . "]", null, 'checkbox'));
 				
-				$tmp = null;
-				$prv = null;
+				$tmp = Array();
+				$prv = Array();
 				$config = $template->getConfig();
 				if(is_array($config['templates'])){
 					foreach($config['templates'] as $name=>$tmpl){
-						$tmp[] = Widget::Anchor(ucfirst(strtolower($name)), "{$this->_uri}/templates/edit/{$template->getHandle()}/{$name}")->generate();
-						$prv[] = Widget::Anchor(ucfirst(strtolower($name)), "{$this->_uri}/templates/preview/{$template->getHandle()}/{$name}/")->generate();
+						if(file_exists(dirname(__FILE__) . '/../templates/' . $template->getHandle() . '/' . $tmpl)){
+							$tmp[] = Widget::Anchor(ucfirst(strtolower($name)), "{$this->_uri}/templates/edit/{$template->getHandle()}/{$name}")->generate();
+							$prv[] = Widget::Anchor(ucfirst(strtolower($name)), "{$this->_uri}/templates/preview/{$template->getHandle()}/{$name}/")->generate();
+						}
 					}
 					$col_template = Widget::TableData(implode(', ',$tmp));
 					$col_preview = Widget::TableData(implode(', ',$prv));
 				}
-				else{
+				if(empty($tmp)){
 					$col_template = Widget::TableData(__('None Found'), 'inactive');
+				}
+				if(empty($prv)){
 					$col_preview = Widget::TableData(__('None Found'), 'inactive');
 				}
 				
@@ -85,6 +94,10 @@ Class contentExtensionemail_templatestemplates extends AdministrationPage {
 		$this->Form->appendChild($actions); 
 	}
 	
+	function __viewNew(){
+		$this->__viewEdit();
+	}
+	
 	function __viewEdit(){
 		if($this->_context[3] == 'saved' || $this->_context[2] == 'saved') {
 			$this->pageAlert(
@@ -107,7 +120,7 @@ Class contentExtensionemail_templatestemplates extends AdministrationPage {
 	}
 	
 	public function __actionEdit() {
-		list(,$handle, $template) = $this->_context;
+		list($mode,$handle, $template) = $this->_context;
 		
 		//Edit Template xsl Files
 		if(!is_null($template)){
@@ -185,13 +198,32 @@ Class contentExtensionemail_templatestemplates extends AdministrationPage {
 			$tpl = str_replace('<!-- TEMPLATES -->', $config['templates'], $tpl);
 			
 			$filename = EmailTemplateManager::getFileNameFromHandle(EmailTemplateManager::getHandleFromName($fields['name']));
-			if(!is_dir(dirname(__FILE__) . '/../templates/' . EmailTemplateManager::getHandleFromName($fields['name'])) && $this->_context[0] == 'edit')
-				@rename(dirname(__FILE__) . '/../templates/' . EmailTemplateManager::getHandleFromName($handle), dirname(__FILE__) . '/../templates/' . EmailTemplateManager::getHandleFromName($fields['name']));
-				@unlink(dirname(__FILE__) . '/../templates/' . EmailTemplateManager::getHandleFromName($fields['name']) . '/' . EmailTemplateManager::getFileNameFromHandle(EmailTemplateManager::getHandleFromName($handle)));
+			if(!is_dir(dirname(__FILE__) . '/../templates/' . EmailTemplateManager::getHandleFromName($fields['name']))){
+				if(is_dir(dirname(__FILE__) . '/../templates/' . EmailTemplateManager::getHandleFromName($handle)) && !empty($handle)){
+					@rename(dirname(__FILE__) . '/../templates/' . EmailTemplateManager::getHandleFromName($handle), dirname(__FILE__) . '/../templates/' . EmailTemplateManager::getHandleFromName($fields['name']));
+					@unlink(dirname(__FILE__) . '/../templates/' . EmailTemplateManager::getHandleFromName($fields['name']) . '/' . EmailTemplateManager::getFileNameFromHandle(EmailTemplateManager::getHandleFromName($handle)));
+				}
+				else{
+					mkdir(dirname(__FILE__) . '/../templates/' . EmailTemplateManager::getHandleFromName($fields['name']));
+				}
+			}
 			$file = dirname(__FILE__) . '/../templates/' . EmailTemplateManager::getHandleFromName($fields['name']) . '/' . $filename;
 			
 			if(!is_writable(dirname($file)) || !$write = General::writeFile($file, $tpl, Symphony::Configuration()->get('write_mode', 'file')))
 				$this->pageAlert(__('Failed to write Template to <code>%s</code>. Please check permissions.', array(dirname(dirname(__FILE__) . '/../templates/'))), Alert::ERROR);
+
+			if($mode == 'new'){
+				$html = file_get_contents(dirname(__FILE__) . '/templates/xsl-html.tpl');
+				$text = file_get_contents(dirname(__FILE__) . '/templates/xsl-text.tpl');
+				
+				$html_file = dirname(__FILE__) . '/../templates/' . EmailTemplateManager::getHandleFromName($fields['name']) . '/template.html.xsl';
+				$text_file = dirname(__FILE__) . '/../templates/' . EmailTemplateManager::getHandleFromName($fields['name']) . '/template.text.xsl';
+				
+				if(!is_writable(dirname($html_file)) || !$write = General::writeFile($html_file, $html, Symphony::Configuration()->get('write_mode', 'file')))
+					$this->pageAlert(__('Failed to write HTML Template to <code>%s</code>. Please check permissions.', array($html_file), Alert::ERROR));
+				if(!is_writable(dirname($text_file)) || !$write = General::writeFile($text_file, $text, Symphony::Configuration()->get('write_mode', 'file')))
+					$this->pageAlert(__('Failed to write HTML Template to <code>%s</code>. Please check permissions.', array($text_file), Alert::ERROR));
+			}
 			
 			redirect($this->_uri . '/templates/edit/' . EmailTemplateManager::getHandleFromName($fields['name']) . '/saved/');
 		}
@@ -294,81 +326,87 @@ Class contentExtensionemail_templatestemplates extends AdministrationPage {
 	}
 	
 	protected function _editClass(){
-		list(,$handle, $template) = $this->_context;
-		if(!empty($handle)){
-			$this->setPageType('form');
-			$this->Form->setAttribute('action', $this->_uri . '/templates/edit/' . $handle . '/');
-			$this->setTitle(__('Symphony &ndash; Email Templates &ndash; Edit'));
-			try{
+		list($mode,$handle, $template) = $this->_context;
+		$this->setPageType('form');
+		$this->Form->setAttribute('action', $this->_uri . '/templates/'.$mode.'/' . $handle . '/');
+		$this->setTitle(__('Symphony &ndash; Email Templates &ndash; Edit'));
+		try{
+			
+			$templ = Array();
+			
+			if($mode != 'new'){
 				$templates = EmailTemplateManager::load($handle);
-					
+				
 				$templ = $templates->getConfig();
 				$templ = (array)$templ['templates'];
-				
-				foreach($templ as $name=>$tm){
+			}
+			
+			foreach($templ as $name=>$tm){
+				if(file_exists(dirname(__FILE__) . '/../templates/' . EmailTemplateManager::getHandleFromName($handle) . '/' . $tm)){
 					$out .= Widget::Anchor(__("Edit ".ucfirst($name)." Template"), $this->_uri . '/templates/edit/' . $handle . '/' . $name, __('Edit Email Template'), 'button', NULL, array('accesskey' => 't'))->generate();
 				}
-				
-				$this->appendSubheading(__($templates->getName() ? $templates->getName() : __('Untitled')),$out);
-				
+			}
+			
+			$this->appendSubheading(__($mode != 'new' ? $templates->getName() : __('Untitled')),$out);
+			
+			$fields = Array();
+			
+			if($mode != 'new'){
 				$fields = $templates->about;
 				$fields = array_merge($fields, $templates->config);
-				
-				$fieldset = new XMLElement('fieldset');
-				$fieldset->setAttribute('class', 'settings');
-				$fieldset->appendChild(new XMLElement('legend', __('Template Settings')));
-
-				$label = Widget::Label(__('Title'));
-				$label->appendChild(Widget::Input(
-					'fields[name]', General::sanitize($fields['name'])
-				));
-
-				if(isset($this->_errors['name'])) {
-					$label = $this->wrapFormElementWithError($label, $this->_errors['name']);
-				}
-
-				$fieldset->appendChild($label);
-				//$this->Form->appendChild($fieldset);
-				
-				$label = Widget::Label(__('Data Sources'));
-
-				$manager = new DatasourceManager($this->_Parent);
-				$datasources = $manager->listAll();
-
-				$options = array();
-
-				if(is_array($datasources) && !empty($datasources)) {
-					if(!is_array($fields['data_sources'])) $fields['data_sources'] = array();
-					foreach ($datasources as $name => $about) $options[] = array(
-						$name, in_array($name, $fields['datasources']), $about['name']
-					);
-				}
-
-				$label->appendChild(Widget::Select('fields[data_sources][]', $options, array('multiple' => 'multiple')));
-				$fieldset->appendChild($label);
-				$this->Form->appendChild($fieldset);
-				
-				$div = new XMLElement('div');
-				$div->setAttribute('class', 'actions');
-				$div->appendChild(Widget::Input(
-					'action[save]', __('Save Changes'),
-					'submit', array('accesskey' => 's')
-				));
-				
-				if($this->_context[0] == 'edit'){
-					$button = new XMLElement('button', __('Delete'));
-					$button->setAttributeArray(array('name' => 'action[delete]', 'class' => 'button confirm delete', 'title' => __('Delete this page'), 'accesskey' => 'd'));
-					$div->appendChild($button);
-				}
-
-				$this->Form->appendChild($div);
 			}
-			catch(EmailTemplateManagerException $e){
-				// TODO: log error?
-				throw new FrontendPageNotFoundException();
+			
+			$fieldset = new XMLElement('fieldset');
+			$fieldset->setAttribute('class', 'settings');
+			$fieldset->appendChild(new XMLElement('legend', __('Template Settings')));
+
+			$label = Widget::Label(__('Title'));
+			$label->appendChild(Widget::Input(
+				'fields[name]', General::sanitize($fields['name'])
+			));
+
+			if(isset($this->_errors['name'])) {
+				$label = $this->wrapFormElementWithError($label, $this->_errors['name']);
 			}
+
+			$fieldset->appendChild($label);
+			//$this->Form->appendChild($fieldset);
+			
+			$label = Widget::Label(__('Data Sources'));
+
+			$manager = new DatasourceManager($this->_Parent);
+			$datasources = $manager->listAll();
+
+			$options = array();
+
+			if(is_array($datasources) && !empty($datasources)) {
+				if(!is_array($fields['data_sources'])) $fields['data_sources'] = array();
+				foreach ($datasources as $name => $about) $options[] = array(
+					$name, in_array($name, (array)$fields['datasources']), $about['name']
+				);
+			}
+
+			$label->appendChild(Widget::Select('fields[data_sources][]', $options, array('multiple' => 'multiple')));
+			$fieldset->appendChild($label);
+			$this->Form->appendChild($fieldset);
+			
+			$div = new XMLElement('div');
+			$div->setAttribute('class', 'actions');
+			$div->appendChild(Widget::Input(
+				'action[save]', __('Save Changes'),
+				'submit', array('accesskey' => 's')
+			));
+			
+			if($this->_context[0] == 'edit'){
+				$button = new XMLElement('button', __('Delete'));
+				$button->setAttributeArray(array('name' => 'action[delete]', 'class' => 'button confirm delete', 'title' => __('Delete this page'), 'accesskey' => 'd'));
+				$div->appendChild($button);
+			}
+
+			$this->Form->appendChild($div);
 		}
-		else{
+		catch(EmailTemplateManagerException $e){
+			// TODO: log error?
 			throw new FrontendPageNotFoundException();
 		}
 	}
