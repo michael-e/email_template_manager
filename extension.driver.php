fa186888a903989b8e28846f7ee695be378bca79
George Petsagourakis <petsagouris@gmail.com>
2012-06-06 22:01:01 +0300

    Started polishing on the extension.driver.php:  - made some strings translatable  - rectified string quotes  - added docblocks, braces, inline commentary  - added an uninstall() to remove the email-templates directory
    Also, in class.emailtemplate.php, fixed Symphony::$Log to Symphony::Log() and some return statements.
    
    Signed-off-by: George Petsagourakis <petsagouris@gmail.com>

--- extension.driver.php
+++ extension.driver.php
@@ -5,6 +5,49 @@ require_once(TOOLKIT.'/class.datasourcemanager.php');
 
 Class extension_email_template_manager extends Extension {
 
+	/**
+	 * The installation logic
+	 *
+	 * We need to make sure that there is a directory named "email-templates" in the workspace.
+	 *
+	 * @return bool
+	 */
+	public function install()
+	{
+
+		// If there isn't any directory like that
+		if (!is_dir(WORKSPACE.'/email-templates')) {
+			// Go ahead and create it.
+			try {
+				mkdir(WORKSPACE.'/email-templates');
+			} catch (Exception $e) {
+				// If we couldn't create it just crap out.
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * The uninstallation logic
+	 *
+	 * We need to remove the "email-templates" directory in the workspace as well as every email template in it.
+	 *
+	 * @return bool|void
+	 */
+	public function uninstall()
+	{
+		General::deleteDirectory(WORKSPACE.'/email-templates');
+	}
+
+	/**
+	 * Add the navigation item for the extension.
+	 *
+	 * Create the "Email Templates" menu item under "Blueprints".
+	 *
+	 * @return array
+	 */
 	public function fetchNavigation()
 	{
 
@@ -17,6 +60,21 @@ Class extension_email_template_manager extends Extension {
 		);
 	}
 
+	/**
+	 * Bind to Symphony's Delegates.
+	 *
+	 * We will be using four delegates to make this work:
+	 *
+	 * On the backend:
+	 * - /blueprints/events/[new|edit]/::AppendEventFilter
+	 * - /blueprint/events/edit/::AppendEventFilterDocumentation
+	 * - /blueprint/datasources/::DataSourceEdit
+	 *
+	 * On the frontend:
+	 * - /frontend/::EventFinalSaveFilter
+	 *
+	 * @return array
+	 */
 	public function getSubscribedDelegates()
 	{
 
@@ -24,51 +82,50 @@ Class extension_email_template_manager extends Extension {
 			array(
 				'page' => '/blueprints/events/edit/',
 				'delegate' => 'AppendEventFilter',
-				'callback' => 'AppendEventFilter'
+				'callback' => 'appendEventFilter'
 			),
 			array(
 				'page' => '/blueprints/events/new/',
 				'delegate' => 'AppendEventFilter',
-				'callback' => 'AppendEventFilter'
+				'callback' => 'appendEventFilter'
+			),
+			array(
+				'page' => '/blueprints/events/edit/',
+				'delegate' => 'AppendEventFilterDocumentation',
+				'callback' => 'appendEventFilterDocumentation'
+			),
+			array(
+				'page' => '/blueprints/datasources/',
+				'delegate' => 'DatasourcePostEdit',
+				'callback' => 'datasourcePostEdit'
 			),
 			array(
 				'page' => '/frontend/',
 				'delegate' => 'EventFinalSaveFilter',
 				'callback' => 'eventFinalSaveFilter'
 			),
-			array(
-				'page' => '/blueprints/events/edit/',
-				'delegate' => 'AppendEventFilterDocumentation',
-				'callback' => 'AppendEventFilterDocumentation'
-			),
-			array(
-				'page' => '/blueprints/datasources/',
-				'delegate' => 'DatasourcePostEdit',
-				'callback' => 'DatasourcePostEdit'
-			),
 		);
 	}
 
-	public function install()
+	/**
+	 * Binding to the DatasourcePostEdit Delegate.
+	 *
+	 * We get the datasource handle and the email templates we have
+	 *
+	 * @uses DatasourcePostEdit http://symphony-cms.com/learn/api/2.3/delegates/#DatasourcePostEdit
+	 *
+	 * @param string $file The path to the Datasource file.
+	 *
+	 * @return bool
+	 */
+	public function datasourcePostEdit($file)
 	{
 
-		if (!is_dir(WORKSPACE.'/email-templates')) {
-			try {
-				mkdir(WORKSPACE.'/email-templates');
-			} catch (Exception $e) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	public function DatasourcePostEdit($file)
-	{
-
-		$ds_handle = DatasourceManager::__getHandleFromFileName(substr($file['file'], strrpos($file['file'], '/') + 1));
+		$ds_handle = DatasourceManager::__getHandleFromFileName(basename($file['file']));
 		$templates = EmailTemplateManager::listAll();
 		foreach ($templates as $template) {
 			$config = $template->getProperties();
+			// TODO: Investigate below, this should not be able to happen since the DatasourcePostEdit just provides array('file' => $file) not array('file' => $file, 'parent' => $parent).
 			if (($key = array_search($file['parent']->Page->_context[1], $config['datasources'])) !== false) {
 				$config['datasources'][$key] = $ds_handle;
 				return EmailTemplateManager::editConfig($template->getHandle(), array_merge($template->getAbout(), $config));
@@ -76,55 +133,105 @@ Class extension_email_template_manager extends Extension {
 		}
 	}
 
-	public function AppendEventFilter($context)
+	/**
+	 * Binding to the AppendEventFilter Delegate.
+	 *
+	 * Attach all the available templates to the available event filters.
+	 *
+	 * @see AppendEventFilter http://symphony-cms.com/learn/api/2.3/delegates/#AppendEventFilter
+	 *
+	 * @param array $context The context data.
+	 *
+	 * @return void
+	 */
+	public function appendEventFilter($context)
 	{
 
 		$templates = EmailTemplateManager::listAll();
 		if (empty($templates)) {
 			return;
 		}
+
+		$tmp = array();
 		foreach ($templates as $template) {
 			$tmp[$template->getHandle()] = $template;
 		}
-		$templates = is_array($tmp) ? $tmp : array();
+
+		$templates = $tmp;
+		unset($tmp);
+
 		ksort($templates, SORT_STRING);
 		foreach ($templates as $template) {
 			$handle = 'etm-'.$template->getHandle();
-			$selected = (in_array($handle, $context['selected']));
-			$context['options'][] = Array(
-				$handle, $selected, General::sanitize("Send Email Template: ".$template->getName())
+			$context['options'][] = array(
+				$handle,
+				(in_array($handle, $context['selected'])),
+				General::sanitize(__('Send Email Template: %s', array($template->getName())))
 			);
 		}
 	}
 
+	/**
+	 * Binding to the EventFinalSaveFilter Delegate.
+	 *
+	 * @see EventFinalSaveFilter http://symphony-cms.com/learn/api/2.3/delegates/#EventFinalSaveFilter
+	 *
+	 * @param array $context The context data
+	 */
 	public function eventFinalSaveFilter($context)
 	{
 
 		$templates = EmailTemplateManager::listAll();
 		foreach ($templates as $template) {
 			$handle = 'etm-'.$template->getHandle();
-			if (in_array($handle, (array)$context['event']->eParamFILTERS)) {
-				if (($response = $this->_sendEmail($template, $context)) !== false) {
-					$context['errors'][] = Array('etm-'.$template->getHandle(), ($response['sent'] > 0), null, $response);
-				}
+			if (in_array($handle, (array)$context['event']->eParamFILTERS)
+				&& (($response = $this->_sendEmail($template, $context)) !== false)
+			) {
+				$context['errors'][] = array(
+					$handle,
+					($response['sent'] > 0),
+					null,
+					$response
+				);
 			}
 		}
 	}
 
+	/**
+	 * Send emails.
+	 *
+	 * Helper function for the EventFinalSaveFilter Delegate binding.
+	 * todo: more descriptive documentation to follow on this. Currently I am too unfamiliar with the meat of the code to do it. -petsagouris
+	 *
+	 * @throws EmailTemplateException
+	 *
+	 * @param EmailTemplate $template
+	 * @param array $context
+	 *
+	 * @return array|bool If everything works, an array of two ints ('total','sent') is returned.
+	 */
 	protected function _sendEmail($template, $context)
 	{
 
 		try {
-			$template->addParams(Array("etm-entry-id" => $context['entry']->get('id')));
-			Symphony::Engine()
-				->Page()->_param["etm-entry-id"] = $context['entry']->get('id');
+			// Make sure that the etm-entry-id is everywhere we will be looking in.
+			$template->addParams(array('etm-entry-id' => $context['entry']->get('id')));
+			Symphony::Engine()->Page()->_param['etm-entry-id'] = $context['entry']->get('id');
+
+			//
 			$xml = $template->processDatasources();
 
 			$about = $context['event']->about();
-			General::array_to_xml($xml, Array("events" => Array($about['name'] => Array("post-values" => $context['fields']))));
+
+			General::array_to_xml($xml, array(
+				'events' => array(
+					$about['name'] => array(
+						'post-values' => $context['fields']
+					)
+				)
+			));
 
 			$template->setXML($xml->generate());
-
 			$template->parseProperties();
 			$properties = $template->getParsedProperties();
 			$recipients = $properties['recipients'];
@@ -133,72 +240,93 @@ Class extension_email_template_manager extends Extension {
 			if (count($recipients) > 0) {
 				foreach ((array)$recipients as $name => $emailaddr) {
 					try {
+						// Get the email object
 						$email = Email::create();
 						$template->addParams(array('etm-recipient' => $emailaddr));
 						$xml = $template->processDatasources();
-
 						$about = $context['event']->about();
-						General::array_to_xml($xml, Array("events" => Array($about['name'] => Array("post-values" => $context['fields']))));
+
+						General::array_to_xml($xml, array(
+							'events' => array(
+								$about['name'] => array(
+									'post-values' => $context['fields']
+								)
+							)
+						));
 
 						$template->setXML($xml->generate());
 						$template->recipients = $emailaddr;
 
 						$content = $template->render();
 
+						// Set the email Subject.
 						if (!empty($content['subject'])) {
-							$email->subject = $content['subject'];
+							$email->setSubject($content['subject']);
 						}
 						else {
-							throw new EmailTemplateException("Can not send emails without a subject");
+							throw new EmailTemplateException(__('Can not send emails without a subject'));
 						}
 
+						// Set the email Reply-To name.
 						if (isset($content['reply-to-name'])) {
-							$email->reply_to_name = $content['reply-to-name'];
+							$email->setReplyToName($content['reply-to-name']);
 						}
 
+						// Set the Reply-To address.
 						if (isset($content['reply-to-email-address'])) {
-							$email->reply_to_email_address = $content['reply-to-email-address'];
+							$email->setReplyToEmailAddress($content['reply-to-email-address']);
 						}
 
+						// Set the plain text part.
 						if (isset($content['plain'])) {
-							$email->text_plain = $content['plain'];
-						}
-						if (isset($content['html'])) {
-							$email->text_html = $content['html'];
+							$email->setTextPlain($content['plain']);
 						}
 
+						// Set the HTML part.
+						if (isset($content['html'])) {
+							$email->setTextHtml($content['html']);
+						}
+
+						// Validate the email address and add it, or throw an exception.
 						require_once(TOOLKIT.'/util.validators.php');
 						if (General::validateString($emailaddr, $validators['email'])) {
-							$email->recipients = array($name => $emailaddr);
+
+							$email->recipients = array(
+								$name => $emailaddr
+							);
 						}
 						else {
-							throw new EmailTemplateException("Email address invalid: $emailaddr");
+							throw new EmailTemplateException(__('Email address invalid: %s',array($emailaddr)));
 						}
 
+						// Send the email.
 						$email->send();
+						// Count it in
 						$sent++;
 					} catch (EmailTemplateException $e) {
-						Symphony::Log()
-							->pushToLog(__('Email Template Manager: ').$e->getMessage(), null, true);
+						// Log the exception, maybe its something we want to have a look at.
+						Symphony::Log()->pushToLog(__('Email Template Manager: ').$e->getMessage(), null, true);
 						continue;
 					}
 				}
 			}
 			else {
-				throw new EmailTemplateException("Can not send an email to nobody, please set a recipient.");
+				throw new EmailTemplateException(__('Can not send an email to nobody, please set a recipient.'));
 			}
-		} catch (EmailTemplateException $e) {
-			$context['errors'][] = Array('etm-'.$template->getHandle(), false, $e->getMessage());
+		}
+		catch (EmailTemplateException $e) {
+			$context['errors'][] = array('etm-'.$template->getHandle(), false, $e->getMessage());
 			return false;
 		}
 		catch (EmailValidationException $e) {
-			$context['errors'][] = Array('etm-'.$template->getHandle(), false, $e->getMessage());
+			$context['errors'][] = array('etm-'.$template->getHandle(), false, $e->getMessage());
 			return false;
 		}
 		catch (EmailGatewayException $e) {
-			$context['errors'][] = Array('etm-'.$template->getHandle(), false, $e->getMessage());
+			$context['errors'][] = array('etm-'.$template->getHandle(), false, $e->getMessage());
 			return false;
 		}
+
 		return array('total' => count($recipients), 'sent' => $sent);
 	}
 }
