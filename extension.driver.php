5de7d0a93ed3fa51d1ee9779f16c530909bf917e
George Petsagourakis <petsagouris@gmail.com>
2012-06-07 22:17:39 +0300

    Reverting the formatting to how it was while keeping the code changes.

--- extension.driver.php
+++ extension.driver.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once(dirname(__FILE__).'/lib/class.emailtemplatemanager.php');
-require_once(TOOLKIT.'/class.datasourcemanager.php');
+require_once(dirname(__FILE__) . '/lib/class.emailtemplatemanager.php');
+require_once(TOOLKIT . '/class.datasourcemanager.php');
 
-Class extension_email_template_manager extends Extension {
+Class extension_email_template_manager extends Extension{
 
 	/**
 	 * The installation logic
@@ -12,20 +12,17 @@ Class extension_email_template_manager extends Extension {
 	 *
 	 * @return bool
 	 */
-	public function install()
-	{
-
+	public function install(){
 		// If there isn't any directory like that
-		if (!is_dir(WORKSPACE.'/email-templates')) {
+		if(!is_dir(WORKSPACE . '/email-templates')){
 			// Go ahead and create it.
-			try {
-				mkdir(WORKSPACE.'/email-templates');
-			} catch (Exception $e) {
+			try{
+				mkdir(WORKSPACE . '/email-templates');
+			} catch(Exception $e){
 				// If we couldn't create it just crap out.
 				return false;
 			}
 		}
-
 		return true;
 	}
 
@@ -36,9 +33,8 @@ Class extension_email_template_manager extends Extension {
 	 *
 	 * @return bool|void
 	 */
-	public function uninstall()
-	{
-		General::deleteDirectory(WORKSPACE.'/email-templates');
+	public function uninstall(){
+		General::deleteDirectory(WORKSPACE . '/email-templates');
 	}
 
 	/**
@@ -48,9 +44,7 @@ Class extension_email_template_manager extends Extension {
 	 *
 	 * @return array
 	 */
-	public function fetchNavigation()
-	{
-
+	public function fetchNavigation(){
 		return array(
 			array(
 				'location' => __('Blueprints'),
@@ -75,8 +69,7 @@ Class extension_email_template_manager extends Extension {
 	 *
 	 * @return array
 	 */
-	public function getSubscribedDelegates()
-	{
+	public function getSubscribedDelegates(){
 
 		return array(
 			array(
@@ -85,14 +78,14 @@ Class extension_email_template_manager extends Extension {
 				'callback' => 'appendEventFilter'
 			),
 			array(
-				'page' => '/blueprints/events/new/',
-				'delegate' => 'AppendEventFilter',
-				'callback' => 'appendEventFilter'
-			),
-			array(
 				'page' => '/blueprints/events/edit/',
 				'delegate' => 'AppendEventFilterDocumentation',
 				'callback' => 'appendEventFilterDocumentation'
+			),
+			array(
+				'page' => '/blueprints/events/new/',
+				'delegate' => 'AppendEventFilter',
+				'callback' => 'appendEventFilter'
 			),
 			array(
 				'page' => '/blueprints/datasources/',
@@ -118,15 +111,13 @@ Class extension_email_template_manager extends Extension {
 	 *
 	 * @return bool
 	 */
-	public function datasourcePostEdit($file)
-	{
-
+	public function datasourcePostEdit($file){
 		$ds_handle = DatasourceManager::__getHandleFromFileName(basename($file['file']));
 		$templates = EmailTemplateManager::listAll();
-		foreach ($templates as $template) {
+		foreach($templates as $template){
 			$config = $template->getProperties();
 			// TODO: Investigate below, this should not be able to happen since the DatasourcePostEdit just provides array('file' => $file) not array('file' => $file, 'parent' => $parent).
-			if (($key = array_search($file['parent']->Page->_context[1], $config['datasources'])) !== false) {
+			if(($key = array_search($file['parent']->Page->_context[1], $config['datasources'])) !== false){
 				$config['datasources'][$key] = $ds_handle;
 				return EmailTemplateManager::editConfig($template->getHandle(), array_merge($template->getAbout(), $config));
 			}
@@ -144,16 +135,12 @@ Class extension_email_template_manager extends Extension {
 	 *
 	 * @return void
 	 */
-	public function appendEventFilter($context)
-	{
-
+	public function appendEventFilter($context){
 		$templates = EmailTemplateManager::listAll();
-		if (empty($templates)) {
-			return;
-		}
+		if(empty($templates)) return;
 
 		$tmp = array();
-		foreach ($templates as $template) {
+		foreach($templates as $template){
 			$tmp[$template->getHandle()] = $template;
 		}
 
@@ -161,8 +148,8 @@ Class extension_email_template_manager extends Extension {
 		unset($tmp);
 
 		ksort($templates, SORT_STRING);
-		foreach ($templates as $template) {
-			$handle = 'etm-'.$template->getHandle();
+		foreach($templates as $template){
+			$handle = 'etm-' . $template->getHandle();
 			$context['options'][] = array(
 				$handle,
 				(in_array($handle, $context['selected'])),
@@ -178,21 +165,14 @@ Class extension_email_template_manager extends Extension {
 	 *
 	 * @param array $context The context data
 	 */
-	public function eventFinalSaveFilter($context)
-	{
-
+	public function eventFinalSaveFilter($context){
 		$templates = EmailTemplateManager::listAll();
-		foreach ($templates as $template) {
-			$handle = 'etm-'.$template->getHandle();
-			if (in_array($handle, (array)$context['event']->eParamFILTERS)
-				&& (($response = $this->_sendEmail($template, $context)) !== false)
-			) {
-				$context['errors'][] = array(
-					$handle,
-					($response['sent'] > 0),
-					null,
-					$response
-				);
+		foreach($templates as $template){
+			$handle = 'etm-' . $template->getHandle();
+			if(in_array($handle, (array)$context['event']->eParamFILTERS)){
+				if(($response = $this->_sendEmail($template, $context)) !== false){
+					$context['errors'][] = array($handle, ($response['sent'] > 0), null, $response);
+				}
 			}
 		}
 	}
@@ -210,26 +190,20 @@ Class extension_email_template_manager extends Extension {
 	 *
 	 * @return array|bool If everything works, an array of two ints ('total','sent') is returned.
 	 */
-	protected function _sendEmail($template, $context)
-	{
+	protected function _sendEmail($template, $context){
 
-		try {
+		try{
 			// Make sure that the etm-entry-id is everywhere we will be looking in.
 			$template->addParams(array('etm-entry-id' => $context['entry']->get('id')));
-			Symphony::Engine()->Page()->_param['etm-entry-id'] = $context['entry']->get('id');
+			Symphony::Engine()
+				->Page()->_param['etm-entry-id'] = $context['entry']->get('id');
 
 			//
 			$xml = $template->processDatasources();
 
 			$about = $context['event']->about();
 
-			General::array_to_xml($xml, array(
-				'events' => array(
-					$about['name'] => array(
-						'post-values' => $context['fields']
-					)
-				)
-			));
+			General::array_to_xml($xml, array('events' => array($about['name'] => array('post-values' => $context['fields']))));
 
 			$template->setXML($xml->generate());
 			$template->parseProperties();
@@ -237,22 +211,16 @@ Class extension_email_template_manager extends Extension {
 			$recipients = $properties['recipients'];
 
 			$sent = 0;
-			if (count($recipients) > 0) {
-				foreach ((array)$recipients as $name => $emailaddr) {
-					try {
+			if(count($recipients) > 0){
+				foreach((array)$recipients as $name => $emailaddr){
+					try{
 						// Get the email object
 						$email = Email::create();
 						$template->addParams(array('etm-recipient' => $emailaddr));
 						$xml = $template->processDatasources();
 						$about = $context['event']->about();
 
-						General::array_to_xml($xml, array(
-							'events' => array(
-								$about['name'] => array(
-									'post-values' => $context['fields']
-								)
-							)
-						));
+						General::array_to_xml($xml, array('events' => array($about['name'] => array('post-values' => $context['fields']))));
 
 						$template->setXML($xml->generate());
 						$template->recipients = $emailaddr;
@@ -260,70 +228,71 @@ Class extension_email_template_manager extends Extension {
 						$content = $template->render();
 
 						// Set the email Subject.
-						if (!empty($content['subject'])) {
+						if(!empty($content['subject'])){
 							$email->setSubject($content['subject']);
 						}
-						else {
+						else{
 							throw new EmailTemplateException(__('Can not send emails without a subject'));
 						}
 
 						// Set the email Reply-To name.
-						if (isset($content['reply-to-name'])) {
+						if(isset($content['reply-to-name'])){
 							$email->setReplyToName($content['reply-to-name']);
 						}
 
 						// Set the Reply-To address.
-						if (isset($content['reply-to-email-address'])) {
+						if(isset($content['reply-to-email-address'])){
 							$email->setReplyToEmailAddress($content['reply-to-email-address']);
 						}
 
 						// Set the plain text part.
-						if (isset($content['plain'])) {
+						if(isset($content['plain'])){
 							$email->setTextPlain($content['plain']);
 						}
 
 						// Set the HTML part.
-						if (isset($content['html'])) {
+						if(isset($content['html'])){
 							$email->setTextHtml($content['html']);
 						}
 
 						// Validate the email address and add it, or throw an exception.
-						require_once(TOOLKIT.'/util.validators.php');
-						if (General::validateString($emailaddr, $validators['email'])) {
+						require_once(TOOLKIT . '/util.validators.php');
+						if(General::validateString($emailaddr, $validators['email'])){
 
 							$email->recipients = array(
 								$name => $emailaddr
 							);
 						}
-						else {
-							throw new EmailTemplateException(__('Email address invalid: %s',array($emailaddr)));
+						else{
+							throw new EmailTemplateException(__('Email address invalid: %s', array($emailaddr)));
 						}
 
 						// Send the email.
 						$email->send();
 						// Count it in
 						$sent++;
-					} catch (EmailTemplateException $e) {
+					} catch(EmailTemplateException $e){
 						// Log the exception, maybe its something we want to have a look at.
-						Symphony::Log()->pushToLog(__('Email Template Manager: ').$e->getMessage(), null, true);
+						Symphony::Log()
+							->pushToLog(__('Email Template Manager: ') . $e->getMessage(), null, true);
 						continue;
 					}
 				}
 			}
-			else {
+			else{
 				throw new EmailTemplateException(__('Can not send an email to nobody, please set a recipient.'));
 			}
 		}
-		catch (EmailTemplateException $e) {
-			$context['errors'][] = array('etm-'.$template->getHandle(), false, $e->getMessage());
+		catch(EmailTemplateException $e){
+			$context['errors'][] = array('etm-' . $template->getHandle(), false, $e->getMessage());
 			return false;
 		}
-		catch (EmailValidationException $e) {
-			$context['errors'][] = array('etm-'.$template->getHandle(), false, $e->getMessage());
+		catch(EmailValidationException $e){
+			$context['errors'][] = array('etm-' . $template->getHandle(), false, $e->getMessage());
 			return false;
 		}
-		catch (EmailGatewayException $e) {
-			$context['errors'][] = array('etm-'.$template->getHandle(), false, $e->getMessage());
+		catch(EmailGatewayException $e){
+			$context['errors'][] = array('etm-' . $template->getHandle(), false, $e->getMessage());
 			return false;
 		}
 
