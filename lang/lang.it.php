3395e9f62ecacb073029a4bbb862eda3157d7372
Davide Grobberio <dadeg90@hotmail.it>
2013-09-04 18:43:28 +0200

    Create lang.it.php

---
+++ lang/lang.it.php
@@ -0,0 +1,54 @@
+<?php
+
+	$about = array(
+		'name' => '',
+		'author' => array(
+			'name' => 'Davide Grobberio',
+			'email' => 'davide@zaniniadv.it',
+			'website' => ''
+		),
+		'release-date' => '2013-09-04'
+	);
+
+	/**
+	 * Email Template Manager
+	 */
+	$dictionary = array(
+
+		'Email Templates' =>
+		false,
+
+		'Email sent successfully.' =>
+		'E-Mail inviata con successo.',
+
+		'Could not save. Please correct errors below.' =>
+		false,
+
+		'Could not save: ' =>
+		false,
+
+		'Could not delete: ' =>
+		false,
+
+		'Body is a required field' =>
+		false,
+
+		'Symphony - Email Templates' =>
+		false,
+
+		'Symphony - Email Templates - %s' =>
+		false,
+
+		'Template updated at %1$s.' =>
+		false,
+
+		'A template with this name already exists.' =>
+    false,
+
+		'This field can not be empty' =>
+    false,
+
+		'Email Template Manager' =>
+    false,
+
+	);
